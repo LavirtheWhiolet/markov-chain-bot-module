5a35b4c888a408b3a3e6e38f3f7316e1cc57efc5
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2014-07-22 18:19:12 +0600

    ...

---
+++ chat_bot_app.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require 'chat_bot'
+require 'auto_marshalling_map'
+require 'optparse'
+
+OptionParser.new do |opts|
+  opts.banner "Usage: #{File.basename(__FILE__)} [options]"
+  opts.separator ""
+  opts.separator "Options may be following:"
+  
+end
